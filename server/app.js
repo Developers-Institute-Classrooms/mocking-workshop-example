a8b38586b7585575064b60309eceadc869889363
Deanne Smeaton <deanne@developersinstitute.ac.nz>
2023-08-08 14:53:05 +1200

    add repo

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+const repository = require("./repository");
 const yaml = require("js-yaml");
 const fs = require("fs");
 const path = require("path");
@@ -23,11 +23,8 @@ app.use("/api/spec", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.post("/api/todos", async (req, res) => {
   try {
     const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
-      [description]
-    );
-    res.status(201).json(newTodo.rows[0]);
+    const newTodo = await repository.addTodo(description);
+    res.status(201).json(newTodo);
   } catch (err) {
     console.error(err.message);
   }
@@ -37,8 +34,8 @@ app.post("/api/todos", async (req, res) => {
 
 app.get("/api/todos", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-    return res.json(allTodos.rows);
+    const allTodos = await repository.getAllTodos();
+    return res.json(allTodos);
   } catch (err) {
     console.error(err.message);
   }
@@ -49,13 +46,11 @@ app.get("/api/todos", async (req, res) => {
 app.get("/api/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
-      id,
-    ]);
-    if (todo.rows[0] === undefined) {
+    const todo = await repository.getTodo(id);
+    if (todo === undefined) {
       res.sendStatus(404);
     } else {
-      res.json(todo.rows[0]);
+      res.json(todo);
     }
   } catch (err) {
     console.error(err.message);
@@ -68,18 +63,15 @@ app.put("/api/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { description } = req.body;
-    const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING todo_id, description",
-      [description, id]
-    );
-    if (updateTodo.rows[0] === undefined) {
+    const updateTodo = await repository.updateTodo(id, description);
+    if (updateTodo === undefined) {
       return res.sendStatus(404);
     }
     if (description === undefined || typeof description !== "string") {
       res.statusMessage = "Request body does not contain a description";
       return res.status(400).end();
     }
-    return res.status(200).json(updateTodo.rows[0]);
+    return res.status(200).json(updateTodo);
   } catch (err) {
     console.error(err.message);
   }
@@ -90,11 +82,8 @@ app.put("/api/todos/:id", async (req, res) => {
 app.delete("/api/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteTodo = await pool.query(
-      "DELETE FROM todo WHERE todo_id = $1 RETURNING todo_id, description",
-      [id]
-    );
-    if (deleteTodo.rows[0] === undefined) {
+    const deleteTodo = await repository.deleteTodo(id);
+    if (deleteTodo === undefined) {
       res.sendStatus(404);
     } else {
       res.status(200).json("Todo was deleted!");
